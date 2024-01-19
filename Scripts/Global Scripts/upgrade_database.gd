4f14d461fc6a1ab6699fb57ceecec7310e757592
un-b <a1848359@adelaide.edu.au>
2024-01-19 18:36:03 +1030

    More setup for upgrades

---
+++ Scripts/Global Scripts/upgrade_database.gd
@@ -0,0 +1,34 @@
+extends Node
+
+#database of upgrades to be applied to the player
+
+
+const UPGRADES = {
+	"upgrade1": {
+		
+		"displayname": "upgrade name",
+		"details" : "does xy to character",
+		#If we want upgrade to follow on from each other, use this
+		"prerequisites": []
+	},
+	
+	"upgrade2": {
+		
+		"displayname": "upgrade name",
+		"details" : "does xy to character",
+		#If we want upgrade to follow on from each other, use this
+		"prerequisites": []
+	},
+	
+	"upgrade3": {
+		
+		"displayname": "upgrade name",
+		"details" : "does xy to character",
+		#If we want upgrade to follow on from each other, use this
+		"prerequisites": []
+	}
+	
+	
+	
+	
+}
--- Scripts/UpgradeOptions.gd
+++ Scripts/UpgradeOptions.gd
@@ -3,11 +3,16 @@ extends ColorRect
 @onready var upgradeName = $Upgr_Name
 @onready var upgradeDescription = $Upgr_Desc
 
-
+#upgrade we choose
+var item=null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if item == "null":
+		item="upgrade1"
+		
+	upgradeName.text=Database.UPGRADES[item]["displayname"]
+	upgradeDescription.text=Database.UPGRADES[item]["details"]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
