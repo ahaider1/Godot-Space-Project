cc132bc00e11a5cc7e5ba0dcd3ec044c152ec979
un-b <a1848359@adelaide.edu.au>
2024-01-24 17:27:28 +1030

    Level 6

--- Scripts/Component Scripts/PathfindComponent.gd
+++ Scripts/Component Scripts/PathfindComponent.gd
@@ -13,22 +13,12 @@ class_name PathfindComponent
 # the vector direction of where the nav_agent is currently pointing at
 @export var current_dir: Vector2
 
-# parent should be an enemy
-@onready var entity: MyEnemyBody = get_parent()
 
 ######### my functions #########
 
 # set the target for the nav_agent
 func setTarget():
 	nav_agent.target_position = target_pos
-	
-	
-	var processed_dir: Vector2
-	processed_dir = nav_agent.get_next_path_position() - global_position
-	processed_dir = processed_dir.normalized()
-	
-	current_dir = processed_dir
-	
 
 
 
@@ -38,6 +28,14 @@ func _ready():
 	EnemyManager.connect("pathfind_signal", _on_pathfind)
 
 
+func _physics_process(_delta):
+	# every physics frame, get the direction that nav_agent points to
+	var processed_dir: Vector2
+	processed_dir = nav_agent.get_next_path_position() - global_position
+	processed_dir = processed_dir.normalized()
+	
+	current_dir = processed_dir
+
 
 
 ######### Godot signal functions #########
@@ -45,8 +43,7 @@ func _ready():
 # whenever pathfind signal is emitted by autoload script
 # we will update position of target
 func _on_pathfind():
-	if entity.has_seen_player:
-		setTarget()
+	setTarget()
 
 
 
