ff8922f0732ad4818c6faef4bac66f3f23ef7904
un-b <a1848359@adelaide.edu.au>
2024-01-25 13:52:02 +1030

    Just some tweaks

--- Scripts/Level Scripts/main_scene.gd
+++ Scripts/Level Scripts/main_scene.gd
@@ -31,11 +31,11 @@ func resetInvAndEquipment():
 		player_equipment.items[i] = null
 	
 	# give the player a default weapon to start
-	#player_inventory.items[0] = BULLET_SHOOTER
+	player_inventory.items[0] = BULLET_SHOOTER
 	#player_inventory.items[1] = TURRET_WEAPON
 	#player_inventory.items[2] = THICK_BLASTER
 	#player_inventory.items[3] = ENERGY_BLASTER
-	player_inventory.items[4] = RAY_BLASTER
+	#player_inventory.items[4] = RAY_BLASTER
 	#
 	#player_inventory.items[5] = FLAME_BLASTER
 	#player_inventory.items[6] = INVERTED_BLASTER
